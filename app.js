d40b1ff2288281d13aa3419966cd83bab3c03378
nvzirv <nazira150800@gmail.com>
2022-03-14 16:32:38 +0300

    new style

---
+++ app.js
@@ -0,0 +1,25 @@
+let body = document.body;
+
+function transformScroll(event) {
+    if (!event.deltaY) {
+      return;
+    }
+  
+    event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+    event.preventDefault();
+  }
+  
+  var element = document.scrollingElement || document.documentElement;
+  element.addEventListener('wheel', transformScroll);
+
+
+
+  
+//   function myFunction() {
+//   var x = document.getElementById("b2");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
